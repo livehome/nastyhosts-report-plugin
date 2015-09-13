4b06382ce81148609643dff6b78604290708850e
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-09-13 02:58:29 +0200

    Make it work better in cluster mode

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 const fs = require("fs");
 
-let reports = [];
-
 function report(req, res) {
-  if(reports.length < 20000 && req.params.ip.length <= 30) {
-    reports.push({
-      ip: req.params.ip,
-      reporter: req.connection.remoteAddress,
-      timestamp: Date.now()
+  res.send("Thanks");
+
+  if(req.params.ip.length <= 45) {
+    fs.stat("reports.txt", function(err, stats) {
+      if(err || stats.size < 20*1024*1024) {
+        const report = JSON.stringify({
+          ip: req.params.ip,
+          reporter: req.connection.remoteAddress,
+          timestamp: Date.now()
+        });
+
+        fs.appendFile("reports.txt", report+"\n");
+      }
     });
   }
-  res.send("Thanks");
 }
 
 module.exports = function(config) {
-  fs.readFile("reports.json", function(err, data) {
-    if(!err) {
-      const prev = JSON.parse(data);
-      if(Array.isArray(prev)) {
-        reports = reports.concat(prev);
-      }
-    }
-  });
-
-  setInterval(function() {
-    fs.writeFile("reports.json", JSON.stringify(reports, null, 2));
-  }, 1000*60);
-
   return {
     routes: [
       {
