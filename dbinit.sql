6225aa7727e9d1db4c2ce6fbf780ab9f0c6221f2
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-10-13 16:08:59 +0200

    Use postgres instead of reports.txt file

---
+++ dbinit.sql
@@ -0,0 +1,31 @@
+create table reported_ips (
+  id bigserial primary key,
+  ip inet unique,
+  reports_count int4 default 0
+);
+
+create table reports (
+  id bigserial,
+  ip_id int4 references reported_ips(id),
+  reporter inet,
+  comment text,
+  timestamp timestamp with time zone default now()
+);
+
+create function addReport(var_ip inet, var_reporter inet, var_comment text) returns void as
+$$
+begin
+  begin
+    insert into reported_ips(ip) values (var_ip);
+  exception when unique_violation then
+    -- do nothing
+  end;
+
+  start transaction;
+    insert into reports(ip_id, reporter, comment) values ((select id from reported_ips where reported_ips.ip = var_ip), var_reporter, var_comment);
+    update reported_ips set reports_count = reports_count + 1 where ip = var_ip;
+  commit;
+end;
+$$
+language plpgsql;
+
--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const fs = require("fs");
+const pg = require("pg");
+let conString;
 
 function report(req, res) {
   res.send("Thanks");
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      console.error("Postgres connection error in report plugin!");
+      return;
+    }
 
-  if(req.params.ip.length <= 45) {
-    fs.stat("reports.txt", function(err, stats) {
-      if(err || stats.size < 20*1024*1024) {
-        const report = JSON.stringify({
+    if(req.params.ip.length <= 45) {
+        const report = {
           ip: req.params.ip,
           reporter: req.connection.remoteAddress,
           comment: req.params.comment,
           timestamp: Date.now()
-        });
+        };
 
-        fs.appendFile("reports.txt", report+"\n");
-      }
-    });
-  }
-}
+        client.query("SELECT addReport($1, $2, $3, $4);", [
+          report.ip,
+          report.reporter,
+          report.comment,
+          report.timestamp
+        ]);
+      };
+    done();
+  });
+};
 
 module.exports = function(config) {
+  conString = config.report.conString;
   return {
     routes: [
       {
