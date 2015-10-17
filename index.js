ad1482ebdf5f102fc65ebde8fe31a121c89fed94
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-10-17 21:58:33 +0200

    Only report not-banned

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 const fs = require("fs");
 const pg = require("pg");
 let conString;
+let ipcheck;
 
 function report(req, res) {
   res.send("Thanks");
-  pg.connect(conString, function(err, client, done) {
-    if(err) {
-      console.error("Postgres connection error in report plugin!");
-      return;
-    }
+  ipcheck(req.params.ip).then(function(res) {
+    if(res.suggestion == "deny") return;
 
-    if(req.params.ip.length <= 45) {
-        const report = {
-          ip: req.params.ip,
-          reporter: req.connection.remoteAddress,
-          comment: req.params.comment,
-          timestamp: Date.now()
+    pg.connect(conString, function(err, client, done) {
+      if(err) {
+        console.error("Postgres connection error in report plugin!");
+        return;
+      }
+
+      if(req.params.ip.length <= 45) {
+          const report = {
+            ip: req.params.ip,
+            reporter: req.connection.remoteAddress,
+            comment: req.params.comment,
+            timestamp: Date.now()
+          };
+
+          client.query("SELECT add_report($1, $2, $3);", [
+            report.ip,
+            report.reporter,
+            report.comment
+          ], function(err) {
+            if(err) console.warn(err)
+          });
         };
-
-        client.query("SELECT add_report($1, $2, $3);", [
-          report.ip,
-          report.reporter,
-          report.comment
-        ], function(err) {
-          if(err) console.warn(err)
-        });
-      };
-    done();
+      done();
+    });
   });
 };
 
-module.exports = function(app, config) {
+module.exports = function(app, config, _ipcheck) {
   conString = config.report.conString;
+  ipcheck = _ipcheck;
   app.get("/report/:ip/:comment", report);
 };
